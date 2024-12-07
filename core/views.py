96ef0acbd3827d0115e2e302965f4ebe19cf5fca
Saurabh Dubey <saurabh.d156@gmail.com>
2024-12-08 04:39:56 +0530

    Changes in html

--- core/views.py
+++ core/views.py
@@ -23,37 +23,44 @@ def category(request):
         'category' : category,
         'category_count' : category_count
     }
-    return render(request, 'categories.html', context)
+    return render(request, 'category/categories.html', context)
 
 def add_category(request):
 
     context= {
         'title' : "Add category"
     }
-    return render(request, 'add-category.html', context)
+    return render(request, 'category/add-category.html', context)
 
 
 # -- ========  Post ======= ---
 def post_list(request):
 
+    posts = Post.objects.all()
+    posts_count = posts.count()
+    featured_image_count = Post.objects.filter(featured_image__isnull=False).count()
+
     context = {
         'title'         : 'Post List',
+        'posts'         : posts,
+        'posts_count'   : posts_count,
+        'featured_image_count' : featured_image_count
     }
-    return render(request, 'post-list.html', context)
+    return render(request, 'post/post-list.html', context)
 
 def post_create(request):
 
     context = {
         'title' : 'Post Create'
     }
-    return render(request, 'post-create.html', context)
+    return render(request, 'post/post-create.html', context)
 
 def post_edit(request):
 
     context = {
         'title' : 'Post Edit'
     }
-    return render(request, 'post-edit.html', context)
+    return render(request, 'post/post-edit.html', context)
 
 
 # -- ========  Author ======= ---
@@ -62,21 +69,21 @@ def author_list(request):
     context = {
         'title' : 'Author List'
     }
-    return render(request, 'author-list.html', context)
+    return render(request, 'author/author-list.html', context)
 
 def author_single(request):
 
     context = {
         'title' : 'Author Single'
     }
-    return render(request, 'author-single.html', context)
+    return render(request, 'author/author-single.html', context)
 
 def edit_profile(request):
 
     context = {
         'title' : 'Edit Profile'
     }
-    return render(request, 'edit-profile.html', context)
+    return render(request, 'author/edit-profile.html', context)
 
 
 # -- ========  Additional ======= ---
@@ -85,12 +92,12 @@ def reviews(request):
     context = {
         'title' : 'Reviews'
     }
-    return render(request, 'reviews.html', context)
+    return render(request, 'additional/reviews.html', context)
 
 def settings(request):
 
     context = {
         'title' : 'Settings'
     }
-    return render(request, 'settings.html', context)
+    return render(request, 'additional/settings.html', context)
 
